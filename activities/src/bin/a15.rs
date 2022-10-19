fbc41467d88c4b2f22518b9f800c73e353e03a95
Ruud Salym J Erie <ruud@oply.co>
2022-10-19 20:44:01 -0300

    completed advanced enum exercise

--- activities/src/bin/a15.rs
+++ activities/src/bin/a15.rs
@@ -10,5 +10,38 @@
 // * Use an enum for the tickets with data associated with each variant
 // * Create one of each ticket and place into a vector
 // * Use a match expression while iterating the vector to print the ticket info
+#[derive(Debug)]
+enum Ticket {
+    Backstage{
+        name: String,
+        price: i32
+    },
+    VIP{
+        name: String,
+        price: i32
+    },
+    Standard{
+        price: i32
+    }
+}
 
-fn main() {}
+
+fn main() {
+    let smartpass = Ticket::Backstage{name: r#"Bob Johnson"#.to_owned(), price:4000};
+    let quickpass = Ticket::Standard{ price:150};
+    let fullpass = Ticket::VIP{name: "Mike Saint Layrent".to_owned(), price:2500};
+    let tickets: Vec<Ticket> = vec![
+        smartpass,
+        quickpass,
+        fullpass
+    ];
+
+for t in tickets {
+    match t {
+        Ticket::Backstage { name, price } => println!("Ticket Info: {:?} {:?} ",name, price),
+        Ticket::VIP { name, price } => println!("Ticket Info: {:?} {:?}",name,price),
+        Ticket::Standard { price } => println!("Ticket Info: {:?} ",price),
+    }
+};
+    
+}
