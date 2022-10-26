aa83400c3a2c68102bf7f77656e5a3a520ed285c
Ruud Salym J Erie <ruud@oply.co>
2022-10-25 23:49:12 -0300

    updated a18b as the instructor changed reqs, old changes saved in a18b_old

--- activities/src/bin/a18b.rs
+++ activities/src/bin/a18b.rs
@@ -1,66 +1,19 @@
-// Topic: Result & the question mark operator
+// Topic: Result
 //
 // Requirements:
-// * Determine if an employee can access a building using a digital keycard
-// * Employees that can access the building are:
-//   * Maintenance crews
-//   * Marketing department employees
-//   * Managers
-// * Other employees that work at the company are:
-//   * Line supervisors
-//   * Kitchen staff
-//   * Assembly technicians
-// * Ensure that terminated employees cannot access the building
-//   regardless of their position
-//
-// Notes:
-// * Use an enum to represent all types of employees
-// * Use a struct to store the employee type and whether they are
-//   still employed
-// * Use a function that returns a Result to determine if the employee
-//   may enter the building
-// * Print whether the employee may access the building
-//   * Must use a function that utilizes the question mark operator to do this
+// * Create an structure named `Adult` that represents a person aged 21 or older:
+//   * The structure must contain the person's name and age
+//   * Implement Debug print functionality using `derive`
+// * Implement a `new` function for the `Adult` structure that returns a Result:
+//   * The Ok variant should contain the initialized structure, but only
+//     if the person is aged 21 or older
+//   * The Err variant should contain a String (or &str) that explains why
+//     the structure could not be created
+// * Instantiate two `Adult` structures:
+//   * One should be aged under 21
+//   * One should be 21 or over
+// * Use `match` to print out a message for each `Adult`:
+//   * For the Ok variant, print any message you want
+//   * For the Err variant, print out the error message
 
-
-enum EmployeeType {
-    MaintenanceCrew,
-    Marketing,
-    Managers,
-    LineSupervisor,
-    KitchenStaff,
-    AssemblyTech
-
-}
-
-struct Employee{
-    current_employed: bool,
-    employee_type: EmployeeType,
-    name: String
-}
-fn grant_access(worker: &Employee) -> Result<bool,String>{
-    if (worker.current_employed) {
-        match worker.employee_type {
-            EmployeeType::MaintenanceCrew => Ok(true),
-            EmployeeType::Marketing => Ok(true),
-            EmployeeType::Managers => Ok(true),
-            _ =>  Err("Access Denied".to_owned()),
-            
-        }
-    } else {
-        Err("Access Denied".to_owned())
-    }
-
-}
-
-
-fn main() {
-    let will = Employee{ current_employed: true, employee_type: EmployeeType::Managers, name: "Will Smith".to_owned() };
-    let jack = Employee{ current_employed: true, employee_type: EmployeeType::LineSupervisor, name: "Carlton Banks".to_owned() };
-
-    let is_access_granted: Result<bool, String> = grant_access(&jack);
-    match is_access_granted{
-        Ok(_) => println!("Access Granted"),
-        Err(_) => println!( "Nah bro ")
-    };
-}
+fn main() {}
