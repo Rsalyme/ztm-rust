5e91ff90a02721fd3d04affb9d0931c9b0138ea4
Ruud Salym J Erie <ruud@oply.co>
2022-10-31 20:40:56 -0300

    added excercise 20

--- activities/src/bin/a20.rs
+++ activities/src/bin/a20.rs
@@ -22,5 +22,74 @@
 // * Use a match expression to convert the user input into the power state enum
 // * The program should be case-insensitive (the user should be able to type
 //   Reboot, reboot, REBOOT, etc.)
+use std::str::FromStr;
+use std::io;
+#[derive(Debug)]
+enum MachineState {
+    OFF,
+    SLEEP,
+    REBOOT,
+    SHUTDOWN,
+    HIBERNATE
+}
 
-fn main() {}
+
+impl FromStr for MachineState {
+
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<MachineState, Self::Err> {
+        let binding = input.to_uppercase();
+        let upper_input= binding.as_str();
+        match &upper_input {
+            &"OFF"  => {
+                println!("Shutting Off");
+                Ok(MachineState::OFF)
+            },
+            &"SLEEP"  => {
+                println!("Going to Sleep");
+                Ok(MachineState::SLEEP)
+            },
+            &"REBOOT" => 
+            {
+                println!("Rebooting....");
+                Ok(MachineState::REBOOT)
+            
+            },
+            &"SHUTDOWN" => {
+                println!("Shutting Down");
+                Ok(MachineState::SHUTDOWN)
+            },            
+            &"HIBERNATE" => {
+                println!("Setting Hibernation Mode");
+                Ok(MachineState::HIBERNATE)
+            },
+            _      => {
+                println!(" I got nothing, Man");
+                
+                Err(())
+            }
+        }
+    }
+}
+
+fn read_input() -> io::Result<String> {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
+    Ok(input.trim().to_owned().to_uppercase())
+}
+
+fn main() {
+    let mut all_input = vec![];
+    let mut times_input = 0;
+    while times_input < 1 {
+        match read_input() {
+            Ok(words) => {
+                MachineState::from_str(words.as_str());
+                all_input.push(words);
+                times_input +=1;
+            },
+            Err(e) => println!("error {:?}",e)
+        }
+    }
+}
